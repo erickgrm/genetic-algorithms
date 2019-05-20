a52340ff4c0420d957be2fac13b791f97849a024
erickgrm <erick.grm@gmail.com>
2019-05-20 15:20:10 -0500

    Not working KNN

--- vnnd/knn/KNN.java
+++ vnnd/knn/KNN.java
@@ -15,33 +15,37 @@ public class KNN{
     public static int D; // dimension
     public static int W; // length of centres 
     public static double[][] training_data;
+    public static double[][] centres;
     public static int N;
 
     // Initialise centres
     public static double[][] startingCentres() {
         double[][] centres = new double[K][D];
+        //for(int k = 0; k < K; k++)
+        //    for(int j = 0; j < D; j++)
+        //        centres[k][j] = Math.random();
+        //return centres;
         for(int k = 0; k < K; k++)
-            for(int i = 0; i < D; i++)
-                centres[k][i] = Math.random();
+            centres[k] = training_data[(int) (N*Math.random())];
         return centres;
     }
 
-    // dists is an arrayy of N x K
-    public static double[][] newCentres(double[][] dists) {
-        double[][] centres = new double[K][D];
-        double jmean;
+    public static double[][] newCentres(double[][] centres) {
+        double[][] clusters = clustering(centres); 
+        double[][] new_centres = new double[K][D];
         int counter;
         for(int k = 0; k < K; k++)
             for(int j = 0; j < D; j++) {
-                jmean = 0.0; counter = 0;
+                counter = 0;
                 for(int i = 0; i < N; i++) 
-                    if(0 < dists[i][k]) {
-                        jmean += dists[i][k];
+                    if(clusters[i][k] == 1) {
+                        new_centres[k][j] += distance(training_data[i], centres[k]);
                         counter += 1;
                     }
-                centres[k][j] = jmean/counter;
+                new_centres[k][j] += centres[k][j];
+                new_centres[k][j] /= (counter + 2);
             }
-        return centres;
+        return new_centres;
     }
 
     public static double[][] clustering(double[][] centres) {
@@ -51,45 +55,59 @@ public class KNN{
                 clusters[i][k] = 0;
         double[][] dists = distances(centres); // dists[N][K]
         for(int i = 0; i < N; i++) {
-            if(dists[i][0] <= dists[i][1] && dists[i][0] <= dists[i][2])
-                clusters[i][0] = 1;
-            if(dists[i][1] <= dists[i][0] && dists[i][1] <= dists[i][2])
-                clusters[i][1] = 1;
-            if(dists[i][2] <= dists[i][0] && dists[i][2] <= dists[i][1])
-                clusters[i][2] = 1;
+            if(dists[i][0] <= dists[i][1]) {
+                if(dists[i][0] <= dists[i][2])
+                    clusters[i][0] = 1;
+                else 
+                    clusters[i][2] = 1;
+            } 
+            else {
+                if(dists[i][1] <= dists[i][2])
+                    clusters[i][1] = 1;
+                else
+                    clusters[i][2] = 1;
+            }
         }
         return clusters;
     }
 
     public static double[][] distances(double[][] centres) {
         double[][] dists = new double[N][K];
-        for(int i = 0; i < N; i++)
-            for(int k = 0; k < K; k++)
+        for(int i = 0; i < N; i++) {
+            System.out.println("Distances to centres");
+            for(int k = 0; k < K; k++) {
                 dists[i][k] = distance(training_data[i], centres[k]);
+                System.out.print(dists[i][k]+" ");
+            }
+        }
         return dists;
     }
     
     public static double distance(double[] p, double[] q) {
+        if(p.length != q.length) {
+            System.out.println("Different size vectors");
+            return -1;
+        }
         double d = 0.0;
         for(int j = 0; j < p.length; j++)
             d += Math.pow(p[j]-q[j], 2);
-        return Math.sqrt(d);
+        return d;
     }
 
     // KNN main routine
-    public static double[][] KNN(double[][] training_data, int K, int G){
+    public KNN(double[][] training_data, int K) {
         KNN.training_data = training_data;
         KNN.K = K; 
         KNN.N = training_data.length; 
         KNN.D = training_data[0].length;
-        
-        double[][] centres = startingCentres();
-        double[][] clusters = new double[N][K]; 
-        double[][] dists new double[N][K];
-        for(int g = 0; g < G; g++) 
-            centres = newCentres(distances(centres));
-        return centres;
     }
+    public static void fit(int G) {
+        double[][] cens = startingCentres();
+        for(int g = 0; g < G; g++) 
+            cens = newCentres(cens);
+        KNN.centres = cens;
+    }
+
     public static void main(String[] args) {
         // Read original data
         double[][] temp = new double[160][16];
@@ -116,18 +134,29 @@ public class KNN{
         }//END read original data
 
         // Build data array, ignore labels
-        double[][] data = new double[160][13];
-        for(int i = 0; i < 160; i++)
+        double[][] data = new double[10][13];
+        for(int i = 0; i < 10; i++)
             for(int j = 0; j < 13; j++)
                 data[i][j] = temp[i][j];
+        double[][] training_data = data;
+        double[][] test_data = data; // No split yet
 
-       double[][] training_data = data;
-       double[][] test_data = data; // No split yet
-       double[][] centres = KNN(training_data, 3, 1);
-       for(int i = 0; i < 3; i++) {
-           for(int j = 0; j < 13; j++)
-               System.out.print(centres[i][j]+" ");
-           System.out.println();
-       }
+        KNN knn = new KNN(training_data, 3);
+        knn.fit(100);
+        double[][] centres = knn.centres;
+        for(int k = 0; k < 3; k++) {
+            for(int j = 0; j < 13; j++)
+                System.out.print(centres[k][j]+" ");
+            System.out.println();
+        }
+        double[][] clusters = clustering(centres);
+        System.out.println("Clusters");
+        int[] counters = new int[K];
+            for(int i = 0; i < 10; i++) {
+                for(int k = 0; k < K; k++) {
+                    System.out.print(clusters[i][k]+" ");
+                }
+                System.out.println();
+            }
     }
 }
